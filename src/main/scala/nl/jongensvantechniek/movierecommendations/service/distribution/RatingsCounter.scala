ea4e7f74c5038d097b5f158e192a456e087cff27
Steve R <steven@jongensvantechniek.nl>
2018-08-21 18:13:07 +0200

    Extend documentation of how ratingscount is performed

--- src/main/scala/nl/jongensvantechniek/movierecommendations/service/distribution/RatingsCounter.scala
+++ src/main/scala/nl/jongensvantechniek/movierecommendations/service/distribution/RatingsCounter.scala
@@ -2,10 +2,25 @@ package nl.jongensvantechniek.movierecommendations.service.distribution
 
 import org.apache.spark.SparkContext
 
+/**
+  * Count how many of each star rating exists in the MovieLens 100K data seet.
+  */
 trait RatingsCounter {
 
   /**
     *  Read in the dataset and compute the distribution of ratings, and sort the result by ratings.
+    *
+    *  Input File format:
+    *
+    *     | userID  movieID  rating  timestamp (UTC epoch time since 01-01-1970) |
+    *
+    *  Example:
+    *
+    *     | 196 242 3  881250949 |
+    *
+    *  Output distribution of ratings:
+    *
+    *     | rating, count(rating) |
     *
     * @param sparkContext the context in which the job will run
     * @param dataSourcePath the dataset for which the distribution is computed
@@ -15,14 +30,16 @@ trait RatingsCounter {
     // Load up each line of the ratings data into an RDD
     val lines = sparkContext.textFile(dataSourcePath)
 
-    // Convert each line to a string, split it out by tabs, and extract the third field.
-    // (The file format is userID, movieID, rating, timestamp)
+    // Convert each line to a string, split it out by tabs, and extract the third field (rating), the ratings will
+    // be outputted as a new RDD
     val ratings = lines.map(x => x.toString.split("\t")(2))
 
     // Count up how many times each value (rating) occurs
+    // The countByValue() is on a RDD is a Spark action, calling this will cause spark, to compute it's DAG and start distributing
+    // and processing all the data in a distributed fashion (on clusters)
     val results = ratings.countByValue()
 
-    // Sort the resulting map of (rating, count) tuples
+    // Sort the resulting map of (rating, count) tuples and return the tuples collection
     results.toSeq.sortBy(_._1)
   }
 }
